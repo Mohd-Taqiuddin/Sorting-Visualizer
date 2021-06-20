e2d3ef43af3c1930197f905446118d33f05b5c8e
Mohd Taqiuddin <taqiuddin12345@gmail.com>
2021-06-20 17:57:19 +0530

    bars display

--- js_files/sorting.js
+++ js_files/sorting.js
@@ -1,12 +1,45 @@
-function newArray() {
-    const array = [];
-    var div = document.createElement('DIV');
-    // console.log('*********')
+let array = [];
+// document.getElementById("newArr").addEventListener("click", newArray);
 
-    for(let i = 0; i < 100; i++){
-        array.push(Math.floor(Math.random() * 100));
-        document.body.appendChild(div);
+// this display array when first opened 
+getArray();
+
+// generate new array
+function getArray(noOfBars = 70) {
+    // deleting old bars
+    deleteChild();
+
+    array = [];
+    for(let i = 0; i < noOfBars; i++){
+        array.push(Math.floor(Math.random() * 250) + 1);
     }
-
     console.log(array);
+
+    const bars = document.querySelector("#bars");
+
+    for(let i = 0; i < noOfBars; i++){
+        const bar = document.createElement("div");
+        bar.style.height = `${array[i] * 2}px`;
+        bar.classList.add('bar');
+        bar.classList.add('flex-item');
+        bar.classList.add(`barNo${i}`);
+        bars.appendChild(bar);
+    }
 }
+
+function deleteChild() {
+    const bar = document.querySelector("#bars");
+    bar.innerHTML = "";
+}
+
+// Selecting newarray button from DOM and adding eventlistener
+const newArray = document.querySelector(".newArray");
+// newArray.addEventListener("click", function(){
+//     console.log("From newArray " + arraySize.value);
+//     console.log("From newArray " + delay);
+//     enableSortingBtn();
+//     enableSizeSlider();
+//     getArray(arraySize.value);
+// });
+console.log(newArray);
+newArray.addEventListener("click",getArray);
